7164d15b10cfa1f065da5d957b90e500e348d37c
Siddhesh <siddheshpatil0533@gmail.com>
2025-02-25 23:21:11 +0530

    DAY38

---
+++ DAY38/Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,46 @@
+public:
+
+    void lpsfind(vector<int>&lps, string s){
+        vector<int>lp(s.size(), 0);
+        int pre=0, suf=1;
+
+        while(suf<s.size()){
+            if(s[pre]==s[suf]){
+                lps[suf]=pre+1;
+                suf++, pre++;
+            }
+            else{
+                if(pre==0){
+                    lps[suf]=0;
+                    suf++;
+                }
+                else{
+                    pre=lps[pre-1];
+                }
+            }
+        }
+    }
+
+    int strStr(string haystack, string needle) {
+
+        vector<int>lps(needle.size(), 0);
+        lpsfind(lps,needle);
+
+        int first=0, second=0;
+
+        while(first< haystack.size()&&second<needle.size()){
+            if(haystack[first]==needle[second]){
+                first++,second++;
+            }
+            else{
+                if(second==0)
+                    first++;
+                else
+                    second=lps[second-1];
+            }
+        }
+        if(second==needle.size())
+        return first-second;
+
+        return -1;
+    }
